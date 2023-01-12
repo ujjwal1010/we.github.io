c60f07045064d3ff3a041e7e47df4ea81d20f7ba
ujjwal1010 <122495809+ujjwal1010@users.noreply.github.com>
2023-01-13 05:40:29 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,155 @@
+//else-if mini mini project
+
+//Simple calculator
+
+/*let choose = parseInt(prompt("Choose one \n1)Add \n2)Subtract \n3)Multiply \n4)Divide"));
+if(choose == 1){
+    let a = parseFloat(prompt("You chose ADD \nEnter your first number"));
+    let b = parseFloat(prompt("Enter your second number"));
+    let sum = parseFloat(a+b);
+    console.log("Sum of two number is " +sum);
+}
+else if(choose == 2){
+    let a = parseFloat(prompt("You chose Subtract \nEnter your first number"));
+    let b = parseFloat(prompt("Enter your second number"));
+    let minus = parseFloat(a-b);
+    console.log("Subtraction of two number is " +minus);
+}
+else if(choose == 3){
+    let a = parseFloat(prompt("You chose Multiply \nEnter your first number"));
+    let b = parseFloat(prompt("Enter your second number"));
+    let times = parseFloat(a*b);
+    console.log("Product of two number is " +times);
+}
+else if(choose == 4){
+    let a = parseFloat(prompt("You chose Division \nEnter your first number"));
+    let b = parseFloat(prompt("Enter your second number"));
+    let divide = parseFloat(a/b);
+    console.log("Division of two number is " +divide);
+}
+else{
+    window.alert("Error!Enter correct number")
+}*/
+
+
+
+
+//Temperature Converter
+
+/*let choose = parseInt(prompt("What do you want to convert \n1)Celsius to Fahrenheit \n2)Fahrenheit to Celcius"));
+if(choose == 1 ){
+    let cel = parseFloat(prompt("You choose Celcius to Fahrenheit \nEnter Your Temperature"));
+    let far = parseFloat((9/5)*cel+32);
+    console.log("Your conversion from celcius to fahrenheit is " + far);
+}
+else if(choose == 2){
+    let far = parseFloat(prompt("You choose Fahrenheit to Celcius \nEnter your Temperature"));
+    let cel = parseFloat(5/9*(far-32));
+    console.log("Your conversion from fahrenheit to celcius is " + cel)
+}
+else{
+    window.alert("Error!Enter correct number")
+}*/
+
+
+
+// password checker
+
+/*let password = prompt("Enter password");
+if(password.length <=4 ){
+    console.log("Weakness disguise me");
+}
+else if(password.length <=6 ){
+    console.log("I mean it's all right");
+}
+else if(password.length <=8 ){
+    console.log("Now,That's what i'm talking about");
+}
+else if(password.length <=10 ){
+    console.log("It's strong as shit");
+}
+else if(password.length >=10 ){
+    console.log("Tooo Long Man");
+}
+else{
+    console.log("You password is safe");
+}*/
+
+
+
+
+//Guessing game
+ 
+/*let guess = parseInt(prompt("Enter a number"));
+if(guess == 1){
+    console.log("You won");
+}
+else{
+    console.log("You loose");
+}*/
+
+
+//Switch Case mini mini project
+
+//Calculator
+ 
+/*let num = parseInt(prompt("Choose one \n1)Add \n2)Subtract \n3)Multiply \n4)Divide"));
+
+switch (num) {
+    case 1:
+    let a = parseFloat(prompt("You chose ADD \nEnter your first number"));
+    let b = parseFloat(prompt("Enter your second number"));
+    let sum = parseFloat(a+b);
+    console.log("Sum of two number is " +sum);
+        break;
+
+    case 2:
+    let c = parseFloat(prompt("You chose Subtraction \nEnter your first number"));
+    let d = parseFloat(prompt("Enter your second number"));
+    let minus = parseFloat(c-d);
+    console.log("Subtraction of two number is " +minus);
+        break;
+
+    case 3:
+    let e = parseFloat(prompt("You chose Multiply \nEnter your first number"));
+    let f = parseFloat(prompt("Enter your second number"));
+    let times = parseFloat(e*f);
+    console.log("Product of two number is " + times);
+        break;
+
+    case 4:
+        let g = parseFloat(prompt("You chose Divide \nEnter your first number"));
+        let h = parseFloat(prompt("Enter your second number"));
+        let divide = parseFloat(g/h);
+        console.log("Division of two number is " +divide);
+            break;            
+    default:
+        window.alert("Error choose righr number");
+        break;
+}*/
+
+
+
+
+//Quiz Game
+
+/*let ans = parseInt(prompt("What was the first programming language? \n1)COBAL \n2)Algorithm "))
+switch (ans) {
+    case 2:
+        window.alert("You are right")
+        break;
+
+    default:
+        window.alert("You are wrong")
+        break;
+}
+let ans2 = parseInt(prompt("Which programming language is father of other languahe? \n1)C \n2)Java \n3)Python"));
+switch (ans2) {
+    case 1:
+        window.alert("You are correct")
+        break;
+
+    default:
+        window.alert("You are wrong")
+        break;
+}*/
